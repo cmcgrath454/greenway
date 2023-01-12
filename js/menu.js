04c3c1417eaac919cb7e4712f828916253d76d6f
Colin McGrath <cmcgrath454@gmail.com>
2023-01-11 19:02:59 -0600

    fixed mobile menu scrolling and some styling

--- js/menu.js
+++ js/menu.js
@@ -2,13 +2,14 @@ const menuToggle = document.querySelector(".mobile-nav-toggle");
 const nav = document.getElementById("main-navigation");
 const closeBtn = document.querySelector(".menu-close");
 const openBtn = document.querySelector(".menu-open");
+const body = document.querySelector("body");
 
 menuToggle.addEventListener('click', () => {
     nav.classList.contains("w-100") ? nav.classList.remove("w-100") : nav.classList.add("w-100");
+    body.classList.contains("fixed-position") ? body.classList.remove("fixed-position") : body.classList.add("fixed-position");
     menuToggle.setAttribute("aria-expanded", !(menuToggle.getAttribute("aria-expanded") == "true"));
     for (const child of menuToggle.children) {
         if (child.nodeName == "IMG")
             child.toggleAttribute("hide");
     }
-    // TODO: Add click outside to close
 })
