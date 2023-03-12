be5ec382b65f72259e444bee1f6d1d138e5acc07
Colin McGrath <cmcgrath454@gmail.com>
2023-03-11 23:45:01 -0600

    added critical inline css generation to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-const { series, parallel, src, dest, watch } = require('gulp');
-const clean = require('gulp-clean');
-const uglify = require('gulp-uglify');
-const sass = require('gulp-sass')(require('sass'));
-const sourcemaps = require('gulp-sourcemaps');
-const cache = require('gulp-cached')
+import gulp from 'gulp';
+import clean from 'gulp-clean';
+import uglify from 'gulp-uglify';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import gulpSourcemaps from 'gulp-sourcemaps';
+import cache from 'gulp-cached';
+import autoprefixer from 'gulp-autoprefixer';
+import { stream as critical } from 'critical';
 
-const isJavaScript = file => { return file.extname === '.js'; };
-const isSass = file => { return file.extname === '.scss'; };
+const sass = gulpSass(dartSass);
+const { series, parallel, src, dest, watch } = gulp;
+const { init, write } = gulpSourcemaps;
 
 function cleanDist() {
     return src('dist/*')
@@ -22,16 +26,17 @@ function minifyJS() {
 
 function buildStyles() {
     return src('src/sass/style.scss')
-        .pipe(sourcemaps.init())
+        .pipe(init())
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer())
         .pipe(dest('dist/css'));
 }
 
 function buildStylesDev() {
     return src('src/sass/style.scss')
-        .pipe(sourcemaps.init())
+        .pipe(init())
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(sourcemaps.write('./maps'))
+        .pipe(write('./maps'))
         .pipe(dest('dist/css'));
 }
 
@@ -41,8 +46,24 @@ function copyOtherFiles() {
         .pipe(dest('dist'));
 }
 
-exports.default = series(cleanDist, parallel(minifyJS, buildStyles, copyOtherFiles));
-exports.dev = function () {
+function criticalCSSInline() {
+    return src('dist/**.html')
+        .pipe(
+            critical({
+                base: 'dist/',
+                inline: true,
+                css: ['dist/css/style.css']
+            })
+        )
+        .on('error', err => {
+            log.error(err.message);
+        })
+        .pipe(gulp.dest('dist'));
+}
+
+const _default = series(cleanDist, parallel(minifyJS, buildStyles, copyOtherFiles), criticalCSSInline);
+export { _default as default };
+export function dev() {
     watch('src/**/*.html', copyOtherFiles);
     watch('src/js/**', minifyJS);
     watch('src/sass/style.scss', buildStylesDev);
